7bf14b0e234ab9a1c2471490580e5b17bb881427
mattbarackman <matthew.barackman@gmail.com>
2012-06-09 14:19:31 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beerswap::Application.config.secret_token = '1e404da332e382aa6730535db9506ba5ee0b4edc52d90ce3c8ca89a5c53dd44e8b5824e26546307eb4c6d808200e7b62a8919adb93852479cfca91674c477b7b'
