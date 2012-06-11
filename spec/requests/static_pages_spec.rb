1d34f9d65ac706496d724a5fadd4c7fd67450e87
mattbarackman <matthew.barackman@gmail.com>
2012-06-10 21:15:15 -0700

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,23 +6,41 @@ describe "Static pages" do
 
     it "should have the content 'Beer Swap'" do
       visit '/static_pages/home'
-      page.should have_content('Beer Swap')
+      page.should have_selector('h1', :text => 'Beer Swap')    
     end
+	it "should have the right title" do
+	  visit '/static_pages/home'
+	  page.should have_selector('title',
+	          :text => "| Home")
+	end
   end
 
   describe "Help page" do
 
     it "should have the content 'Help Page'" do
       visit '/static_pages/help'
-      page.should have_content('Help Page')
-    end
+      page.should have_selector('h1', :text => 'Help Page')    
+  	end
+	it "should have the right title" do
+	  visit '/static_pages/help'
+	  page.should have_selector('title',
+	          :text => "| Help")
+	end
   end
 
   describe "About page" do
 
     it "should have the content 'About me'" do
       visit '/static_pages/about'
-      page.should have_content('About me')
+      page.should have_selector('h1', :text => 'About me')
     end
+	
+	it "should have the right title" do
+	  visit '/static_pages/about'
+	  page.should have_selector('title',
+	          :text => "| About")
+	end
   end 
+
+
 end
